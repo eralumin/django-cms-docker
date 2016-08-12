b85de555066c0050b7a5f3f91d4967f2b3fadb15
Daimebag <kevin.marilleau@gmail.com>
2016-08-12 21:54:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,11 +6,9 @@ echo "$?"
 if [ $? -ne 0 ]; then
 echo "if"
 #	python manage.py runserver
-/bin/bash
 else
 echo "else"
 	djangocms -f -p . myapp
 #	python manage.py runserver
 echo "$?""
-/bin/bash
 fi
