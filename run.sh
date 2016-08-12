07a5907c132de354f3d10434da0c8210b9118947
Daimebag <kevin.marilleau@gmail.com>
2016-08-12 20:23:12 +0200

    Create run.sh
    For test install of djangocms before launch it

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 							\
+	djangocms -f -p -q . myapp &> /dev/null \
+	if [ $? -ne 0 ]; then 					\
+		python manage.py runserver			\
+	else									\
+		djangocms -f -p . myapp				\
+		python manage.py runserver			\	
+	fi
