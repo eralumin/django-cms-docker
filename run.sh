e89804254da2ad1cc73ccb028358765dbee93758
Daimebag <kevin.marilleau@gmail.com>
2016-08-13 17:22:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 
 
 if test "$(ls -A "/usr/src/app" &> /dev/null)"; then
-	python manage.py runserver 0.0.0.0:8000
+#	python manage.py runserver 0.0.0.0:8000
+	/bin/sh
 else
 	read -p "Project Name: " projectname
 	djangocms -f -p . $projectname
-	python manage.py runserver 0.0.0.0:8000
+#	python manage.py runserver 0.0.0.0:8000
+	/bin/sh
 fi
