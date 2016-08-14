70f822d8a6108131a95225fa902d3f87364e3b08
Daimebag <kevin.marilleau@gmail.com>
2016-08-14 13:23:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 
 if test "$(ls -A "/usr/src/app" &> /dev/null)"; then
+	pip install --no-cache-dir -r requirements.txt
 	python manage.py runserver 0.0.0.0:8000
 else
 	read -p "Project Name: " projectname
