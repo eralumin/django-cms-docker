d1265272a6b3a5473ab1bc47a8adeb5b380abea5
k_marilleau <kevin.marilleau@gmail.com>
2017-01-02 21:15:22 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 
-if test "$(ls -A "/usr/src/app" &> /dev/null)"; then
-	pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 #	python manage.py runserver 0.0.0.0:8000
-	/bin/sh
-else
-	read -p "Project Name: " projectname
-	djangocms -f -p . $projectname
-#	python manage.py runserver 0.0.0.0:8000
-	/bin/sh
-fi
+/bin/sh
